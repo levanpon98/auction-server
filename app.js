0dc4db0f9298c9887826fbca12cdac369cfa9252
levanpon1009 <levanpon1009@gmail.com>
2020-01-06 01:01:22 +0700

    Update

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(cors({
 }));
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ const productSchema = mongoose.Schema({
     },
     title: { type: String, required: true},
     price: {type: Number, required: true},
-    image: {type: String},
+    image: {type: String, required: true},
     description: {type: String, required: true},
     status: Number
 });

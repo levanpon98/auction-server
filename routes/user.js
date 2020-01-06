d11fa9b4c53bfc13eb56a3575ac965326e349677
levanpon1009 <levanpon1009@gmail.com>
2020-01-06 14:37:58 +0700

    Update

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ router.get('/check-token', UserController.check_token);
 router.post('/register', UserController.signup);
 router.delete('/:id', UserController.delete);
 router.get('/:id', checkAuth, UserController.get_user_by_id);
-router.get('/', UserController.get_all_user);
+router.get('/', checkAuth, UserController.get_all_user);
 router.get('/view-profile/:id', checkAuth, UserController.get_user_for_view);
 router.patch('/:id', checkAuth, UserController.update_user_by_id);
 router.post("/forgot-password", UserController.forgot_password);
